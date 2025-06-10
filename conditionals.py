477fc64609cf832c6e84c4c3b725152d54793ded
Nikesh Jung Shrestha <140252517+NikeshJS1@users.noreply.github.com>
2025-06-10 19:19:23 +0545

    Delete conditionals.py

--- conditionals.py
+++
@@ -1,7 +0,0 @@
-marks = float(input("Enter your marks percentage: "))
-if(marks >= 40.0):
-    print("You have passed the exam. Congratulations!")
-elif(marks < 10.0):
-    print("This is so low. You have failed horribly! You need to take your academics seriously!")
-else:
-    print("You have failed.")
