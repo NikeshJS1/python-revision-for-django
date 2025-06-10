68414d8921bb2dfb83e52618be28eb2bccbb6309
NikeshJS1 <nikeshjs123@gmail.com>
2025-06-10 19:38:12 +0545

    Conditionals revision

---
+++ conditionals.py
@@ -0,0 +1,6 @@
+marks = float(input("Enter your percentage: "))
+if(marks >= 40.0):
+    print("You have successfully passed the exam. Congratulations!")
+else:
+    print("Unfortunately, you are a failure.")
+    
